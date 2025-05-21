7d599dbcf957cad4a08ba6dea30f4baf4e895156
dehuihu <dehuihu@gmail.com>
2025-05-21 10:53:40 -0700

    Co-authored-by: Flora <FloritaZhao@users.noreply.github.com>

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,8 @@
-export const mystery1 = (num) => {};
+export const mystery1 = (num) => {
+  // if (num % 2 === 0) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  return num % 2;
+};
